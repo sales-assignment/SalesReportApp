fdab4baa1728119c028cabd1a2bdfb61d5cc3f1e
Ghraz-hub <tomas.dan.carlsson@hotmail.com>
2026-08-28 16:05:53 +0200

    Changed if-structure

--- SalesReportApp/SalesLogic.cs
+++ SalesReportApp/SalesLogic.cs
@@ -6,6 +6,7 @@ namespace SalesReportApp;
 
 internal class SalesLogic
 {
+
     public static void WelcomeMessage()
     {
         bool isRunning = true;
@@ -21,34 +22,35 @@ internal class SalesLogic
 
             string? userInput = Console.ReadLine();
 
-            Console.Write("Ange försäljningsbelopp: ");
-
-            bool input = decimal.TryParse(Console.ReadLine(), out decimal value);
-
-            if(string.IsNullOrWhiteSpace(userInput) || input == false || value == 0 )
+            if (string.IsNullOrWhiteSpace(userInput))
             {
                 Console.WriteLine("Du har skrivit in fel, försök igen!");
             }
-            else if (userInput.ToLower() == "klar")
+            else if (userInput.ToLower()== "klar")
             {
                 isRunning = false;
             }
             else
             {
-                productList.Add(userInput);
-                priceList.Add(value);
-                totalProducts++;
+                Console.Write("Ange försäljningsbelopp: ");
+                bool input = decimal.TryParse(Console.ReadLine(), out decimal value);
+                if (input == false || value <= 0)
+                {
+                    Console.WriteLine("Du har skrivit in fel, försök igen!");
+                }
+                else
+                {
+                    productList.Add(userInput);
+                    priceList.Add(value);
+                    totalProducts++;
+                }
             }
-
             for (int i = 0; i < productList.Count; i++)
             {
                 Console.WriteLine("");
                 Console.WriteLine($"{ productList[i]}: {priceList[i]}kr.");
                 Console.WriteLine("");
             }
-
-
-
         }
         while(isRunning);
     }
